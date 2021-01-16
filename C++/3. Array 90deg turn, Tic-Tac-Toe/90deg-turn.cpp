0beaab8bc768374edf0f01d23272bee9071e8cba
Ervīns Malnačs <bilijs1997@gmail.com>
2021-01-16 21:47:51 +0200

    Initial commit

---
+++ C++/3. Array 90deg turn, Tic-Tac-Toe/90deg-turn.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+
+int main () {
+	time_t t;
+	srand((unsigned) time(&t));
+	const int M = 5;
+	int A[M][M];
+	
+
+	// Aizpildisana
+	for(int i = 0; i < M; i++) {
+        for(int j = 0; j < M; j++) {
+            A[i][j] = (rand() % 90) + 10;
+		}
+	}
+    
+    // Izvade
+    std::cout << "Orginala matrica:\n";
+    for (int i = 0; i < M; i++) {
+		for (int j = 0; j < M; j++) {
+			std::cout << A[i][j] << " ";
+		}
+		std::cout << "\n";
+	}
+	
+	// Pagriesana
+	std::cout << "\nPagriezta matrica:\n";
+	for(int i = 0; i < M; i++) {
+    	for (int j = 0; j < M; j++) {
+			std::cout << A[M - 1 - j][i] << " ";
+		}
+		std::cout << "\n";
+	}
+  
+	std::cout << "\n";
+	
+
+	system("Pause");
+	return 0;
+}
