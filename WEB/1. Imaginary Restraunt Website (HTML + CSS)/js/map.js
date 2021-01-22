a97fe99a6adf577b557d23d0773c232b226028bf
Ervīns Malnačs <bilijs1997@gmail.com>
2021-01-22 22:34:46 +0200

    Commit

---
+++ WEB/1. Imaginary Restraunt Website (HTML + CSS)/js/map.js
@@ -0,0 +1,8 @@
+function myMap() {
+	var mapProp={
+		center:new google.maps.LatLng(56.937958, 24.157069),
+		zoom:5,}
+	;
+	
+	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
